5f4e4066c2369ffd0080c501020c69a92283421d
u1810291 <otabekbutcher@gmail.com>
2021-09-23 21:52:12 +0500

    Initialized repository and configured eslint config

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,5 +1,5 @@
 import { ReportHandler } from 'web-vitals';
-
+// eslint-ignore-next-line
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
---
+++ src/root/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      Hello world
+      {console.log('object')}
+    </header>
+  </div>
+);
+
+export default App;
